ca902ac366d1cc35918d34152db0ee56649dbb57
lynas <sazzad@ipvision-inc.com>
2015-04-08 15:42:36 +0600

    some new file added

--- git.py
+++ git.py
@@ -2,7 +2,7 @@ import subprocess
 commitMessage ="git commit -m \"" + raw_input("your commit message \n") + "\""
 
 commands =[
-    "git add .",
+    "git add --all",
     commitMessage,
     "git push"
 ]
